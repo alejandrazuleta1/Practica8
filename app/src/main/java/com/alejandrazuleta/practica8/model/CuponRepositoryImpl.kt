0d3529e918db9bb038428b4603e2893e6ebcc88b
Alejandra <alejandra.zuletag1@udea.edu.co>
2020-04-27 11:19:20 -0500

    cambio de llave por exceso de peticiones y agregué al manifest la instruccion

--- app/src/main/java/com/alejandrazuleta/practica8/model/CuponRepositoryImpl.kt
+++ app/src/main/java/com/alejandrazuleta/practica8/model/CuponRepositoryImpl.kt
@@ -8,7 +8,8 @@ import retrofit2.Response
 
 class CuponRepositoryImpl(var cuponPresenter: CuponPresenter): CuponRepository{
     override fun loadListCupones() {
-        val apiKey="16ab4d84f2da9b70874015feedce8e69"
+        //val apiKey="16ab4d84f2da9b70874015feedce8e69"
+        val apiKey="beabfcd2f9cf74f624230f27f377b5d1"
         val format="json"
 
         ApiService.create()
